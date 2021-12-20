22347f5d14fc1b3d22cc561d70e4d7714cdd0e33
Marcos paulo arruda junior <juninho1_2010@hotmail.com>
2021-12-20 18:29:44 -0300

    Actions pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route ('/', methods=['GET'])
 def home ():
-    return "<h1>Flask App na Ecs rodando via pipeline</h1>"
+    return "<h1>Teste executado a partir do github actions</h1>"
 
 if __name__== '__main__':
     app.run(debug=True,host="0.0.0.0")
